06f810ee7b87e35f738db0121b07c5758144b013
ayakawamura <ayakawamura.b@gmail.com>
2021-11-07 14:42:24 +0000

    [Add]ルーティング

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
-  has_many :relationships, class_name: 'Relationships', foreign_key: follower_id, dependent: :destroy
+  has_many :relationships, class_name: 'Relationships', foreign_key: 'follower_id', dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
-  has_many :reverse_of_relationships, class_name: 'Relationships', foreign_key: followed_id, dependent: :destroy
+  has_many :reverse_of_relationships, class_name: 'Relationships', foreign_key: 'followed_id', dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   
   validates :name, presence: true, uniqueness: true
