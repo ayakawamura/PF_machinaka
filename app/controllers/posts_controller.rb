e05ae06c70d2334e9733051366afccc7ca8f1b11
ayakawamura <ayakawamura.b@gmail.com>
2021-11-08 09:31:05 +0000

    [Add]ヘッダー、ユーザーサインアップ、ログイン、編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,10 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index,:show]
+  
+  def index
+  end
+  
+  def show
+  end  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get 'searches/user_search'
   get 'searches/tag_search'
   get 'homes/about'
+  get 'timeline' => 'posts#timeline'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
