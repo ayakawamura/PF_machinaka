491ea90e57c0f9bbe45168dce8cd91c0dc008f0c
ayakawamura <ayakawamura.b@gmail.com>
2021-11-08 13:08:05 +0000

    [update]migrationファイル

--- db/migrate/20211108082413_create_tag_relationships.rb
+++ db/migrate/20211108082413_create_tag_relationships.rb
@@ -2,8 +2,8 @@ class CreateTagRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :tag_relationships do |t|
       t.references :post, null: false, foreign_key: true
-      t.references :tag, null: false, foreign_key: true
 
+      t.references :tag, null: false, foreign_key: true
       t.timestamps
     end
     add_index :tag_relationships, [:post_id, :tag_id], unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_091051) do
+ActiveRecord::Schema.define(version: 2021_11_08_082413) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
