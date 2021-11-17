416c7d89bc274f3ff167f26707a47eb02b68b7ff
ayakawamura <ayakawamura.b@gmail.com>
2021-11-17 03:51:20 +0000

    [Update]turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,8 +22,8 @@
 //= require_tree .
 
 // トップページ、メイン画像
-$(document).ready(function () {
-  $("#images").skippr({
+$(document).on('turbolinks:load',function () {
+  $("#top-images").skippr({
     // スライドショーの変化 ("fade" or "slide")
     transition : 'fade',
     // 変化に係る時間(ミリ秒)
@@ -45,10 +45,33 @@ $(document).ready(function () {
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
     hidePrevious : false
   });
+
+  $("#show-images").skippr({
+    // スライドショーの変化 ("fade" or "slide")
+    transition : 'fade',
+    // 変化に係る時間(ミリ秒)
+    speed : 2000,
+    // easingの種類
+    easing : 'easeOutQuart',
+    // ナビゲーションの形("block" or "bubble")
+    navType : 'bubble',
+    // 子要素の種類('div' or 'img')
+    childrenElementType : 'div',
+    // ナビゲーション矢印の表示(trueで表示)
+    arrows : true,
+    // スライドショーの自動再生(falseで自動再生なし)
+    autoPlay : true,
+    // 自動再生時のスライド切替間隔(ミリ秒)
+    autoPlayDuration : 7000,
+    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
+    keyboardOnAlways : false,
+    // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
+    hidePrevious : false
+  });
 });
 
 // arrowアイコン
-$(function() {
+$(document).on('turbolinks:load',function() {
   $('#arrow a').on('click',function(event){
     $('body, html').animate({
       scrollTop:0
@@ -58,7 +81,7 @@ $(function() {
 });
 
 // ハンバーガーmenu
-$(function() {
+$(document).on('turbolinks:load',function() {
   $('.menu-trigger').on('click', function(event) {
     $(this).toggleClass('active');
     $('#sp-menu').fadeToggle();
