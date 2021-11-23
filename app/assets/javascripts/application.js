d2502ddfc8050cee59b77388c97e54a8da505bc6
ayakawamura <ayakawamura.b@gmail.com>
2021-11-23 08:11:50 +0000

    [Update]SCSSへ修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,26 +47,16 @@ $(document).on('turbolinks:load',function () {
   });
 
   $("#show-images").skippr({
-    // スライドショーの変化 ("fade" or "slide")
     transition : 'fade',
-    // 変化に係る時間(ミリ秒)
-    speed : 2000,
-    // easingの種類
+    speed : 1000,
     easing : 'easeOutQuart',
-    // ナビゲーションの形("block" or "bubble")
-    navType : 'bubble',
-    // 子要素の種類('div' or 'img')
+    navType : 'bubble', 
     childrenElementType : 'div',
-    // ナビゲーション矢印の表示(trueで表示)
     arrows : true,
-    // スライドショーの自動再生(falseで自動再生なし)
-    autoPlay : true,
-    // 自動再生時のスライド切替間隔(ミリ秒)
+    autoPlay : false,
     autoPlayDuration : 7000,
-    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
-    keyboardOnAlways : false,
-    // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
-    hidePrevious : false
+    keyboardOnAlways : true,
+    hidePrevious : true
   });
 });
 
